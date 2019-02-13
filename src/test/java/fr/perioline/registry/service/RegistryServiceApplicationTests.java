334d69396dd5ec83ddd1183ce93e53d4e89c782e
youssef smimaa <youssef.smimaa@gmail.com>
2019-02-13 10:55:49 +0100

    kick off

---
+++ src/test/java/fr/perioline/registry/service/RegistryServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.perioline.registry.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RegistryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
