036ae096ae785d17be54132bc8f3a43b3b76f301
youssef smimaa <youssef.smimaa@gmail.com>
2019-02-18 12:06:37 +0100

    update registy config

--- src/main/java/fr/perioline/registry/service/RegistryServiceApplication.java
+++ src/main/java/fr/perioline/registry/service/RegistryServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RegistryServiceApplication.class, args);
 	}
