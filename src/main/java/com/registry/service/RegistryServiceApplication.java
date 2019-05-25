529219c4032f8d1b605e05ad81cca30f64538ae8
youssef smimaa <youssef.smimaa@gmail.com>
2019-05-25 13:00:47 +0200

    [service-resgitry] update appilcation.properties

--- src/main/java/com/registry/service/RegistryServiceApplication.java
+++ src/main/java/com/registry/service/RegistryServiceApplication.java
@@ -1,8 +1,7 @@
-package fr.perioline.registry.service;
+package com.registry.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
