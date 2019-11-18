af3e0dd420a306f361b7fbc092cad3ce2107567a
youssef smimaa <youssef.smimaa@gmail.com>
2019-11-19 00:02:45 +0100

    [service-registry] update app name and some config param

--- src/main/java/com/service/registry/RegistryServiceApplication.java
+++ src/main/java/com/service/registry/RegistryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.registry.service;
+package com.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
